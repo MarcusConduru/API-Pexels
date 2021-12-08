f8f2ad92ad3647ddfcb1336a306d510d9b23defc
Marcus Vinicius <marcus.conduru@gmail.com>
2021-12-08 18:34:01 -0300

    feat: ensure a new component footer

---
+++ src/Presentation/Components/Footer/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './footer-styles.scss'
+
+const Footer = () => {
+  return (
+   <footer className='Footer__content'>
+     <p className='Footer__text'>&copy; Copyright 2021-2021 Leadster</p>
+   </footer>
+  )
+}
+
+export default Footer
