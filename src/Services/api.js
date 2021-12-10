aaafd92aed7a4aa72876320c0ee8a79b50bfafef
Marcus Vinicius <marcus.conduru@gmail.com>
2021-12-09 23:36:56 -0300

    feat: ensure adjust API consumi

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '',
+  baseURL: 'https://api.pexels.com/v1',
   headers: {
-    authorization: ''
+    authorization: '563492ad6f91700001000001b6b33a477eee4c5ba06322bd76272386'
   }
 })
 
