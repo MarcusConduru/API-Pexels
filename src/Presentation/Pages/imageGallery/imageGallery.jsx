b220246662f83f749798d83b07dad0bc27204032
Marcus Vinicius <marcus.conduru@gmail.com>
2021-12-09 23:39:19 -0300

    feat: ensure lazy loading and introduction API in page

--- src/Presentation/Pages/imageGallery/imageGallery.jsx
+++ src/Presentation/Pages/imageGallery/imageGallery.jsx
@@ -1,15 +1,60 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './imageGallery-styles.scss'
 import { Footer, Header } from '../../Components'
+import { getGalleryImage,getGalleryImageSearch,getGalleryImageDetail } from '../../../Services/events'
+import { useHistory, useLocation } from 'react-router-dom/cjs/react-router-dom.min'
 
 const ImageGallery = () => {
+  const [imageGallery, setImageGallery] = useState([]);
+  const [error, setError] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  const [perPage,setPerPage] = useState(15)
+  const history = useHistory()
+  const location = useLocation()
+
+  useEffect(() => {
+    getGalleryImage(perPage).then(response => {
+      setImageGallery(response)  
+      setIsLoading(true)
+    }).catch(error => {
+      setError(error.message)
+    })
+  },[])
+
+  useEffect(() => {
+    const intersectionObserver = new IntersectionObserver((entry) => {
+      if(entry.some(entries => entries.isIntersecting)) {
+        setPerPage((PerPageElement) => PerPageElement + 15)
+      }
+    })
+
+    intersectionObserver.observe(document.getElementById('observer'))
+
+    return () => intersectionObserver.disconnect()
+  },[])
+
+  const handleSeachImage = () => {
+    history.push({
+      pathname: 's'
+    })
+  }
+
   return (
     <div className='Gallery__container'>
       <Header />
       <section className='Gallery__content'>
         {Array(17).fill().map((el,index) => (
           <div key={index} className='Gallery__box'>
-            <img src="https://images.unsplash.com/photo-1433360405326-e50f909805b3?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=359e8e12304ffa04a38627a157fc3362" className='Gallery__img' />
+            <img 
+              srcset="
+                https://images.unsplash.com/photo-1609058745811-2e87ab15790a?w=800 800w,
+                https://images.unsplash.com/photo-1608749333098-a72487cff005?w=1000 1000w,
+                https://images.unsplash.com/photo-1601758176559-76c75ead317a?w=1250 1250w,
+                https://images.unsplash.com/photo-1609017879390-e1968a80ec6e?w=1500 1500w,
+                https://images.unsplash.com/photo-1609027910482-839beaea931e?w=1750 1750w,
+                https://images.unsplash.com/photo-1609008804635-571054f8d863?w=2000 2000w"
+              className='Gallery__img' 
+            />
             <div className='Gallery__photographer'>
               <p className='Gallery__title'>Fotogrado por:</p>
               <p className='Gallery__name'>jose lima de dois</p>
@@ -18,6 +63,7 @@ const ImageGallery = () => {
         ))}
       </section>
       <Footer />
+      <div id='observer' />
     </div>
   )
 }
