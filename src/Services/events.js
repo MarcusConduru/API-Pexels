9fdf2d3ae8cb56fbf781bbb2675f299e4bc75a37
Marcus Vinicius <marcus.conduru@gmail.com>
2021-12-09 23:36:11 -0300

    feat: ensure entering all routes

--- src/Services/events.js
+++ src/Services/events.js
@@ -1,13 +1,27 @@
-import getMethod from './MethodsAxios'
-import { UnexpectedError,NotFound } from '../Validation/Errors'
+import { getMethod } from './MethodsAxios'
+import { UnexpectedError, NotFound, AccessDeniedError } from '../Validation/Errors'
 
-const getGalleryImage = () => {
-  const response = getMethod('')
-
-  switch (response) {
+const errorDeal = response => {
+  switch (response.status) {
     case 200: return response.data 
     case 204: return []
+    case 403: throw new AccessDeniedError()
     case 404: throw new NotFound() 
     default: throw new UnexpectedError()
   }
+}
+
+export const getGalleryImage = async (per_page) => {
+  const response = await getMethod('curated', { params: { per_page: per_page} })
+  return errorDeal(response)
+}
+
+export const getGalleryImageDetail = async (id) => {
+  const response = await getMethod(`photos/${id}`)
+  return errorDeal(response)
+}
+
+export const getGalleryImageSearch = async (per_page,queryParams) => {
+  const response = await getMethod(`search`, { params: { per_page: per_page,  query: queryParams } })
+  return errorDeal(response)
 }
