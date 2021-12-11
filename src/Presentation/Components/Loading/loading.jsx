72ac703a2ff800b6a2d2c51fce10c5c396fd123c
Marcus Vinicius <marcus.conduru@gmail.com>
2021-12-11 01:25:48 -0300

    feat: ensure add a new component loading

---
+++ src/Presentation/Components/Loading/loading.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+import './loading-styles.scss'
+
+const Loading = () => {
+  return (
+    <div className="Loading__container">
+      <div className="Loading__item" />
+    </div>
+  )
+}
+
+export default Loading
