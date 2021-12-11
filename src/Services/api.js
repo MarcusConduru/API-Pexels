5e2b1ddc520599b3491ff9c46ea781eb77eb30dc
Marcus Vinicius <marcus.conduru@gmail.com>
2021-12-11 01:24:45 -0300

    feat: ensure add UserInvalidError exception

--- src/Services/api.js
+++ src/Services/api.js
@@ -3,8 +3,8 @@ import axios from 'axios'
 const api = axios.create({
   baseURL: 'https://api.pexels.com/v1',
   headers: {
-    authorization: '563492ad6f91700001000001b6b33a477eee4c5ba06322bd76272386'
+    authorization: '563492ad6f91700001000001a627014a4ed34c1286042d5326d0baed'
+    // Chave Da Api
   }
 })
-
 export default api
--- src/Services/events.js
+++ src/Services/events.js
@@ -1,10 +1,11 @@
 import { getMethod } from './MethodsAxios'
-import { UnexpectedError, NotFound, AccessDeniedError } from '../Validation/Errors'
+import { UnexpectedError, NotFound, AccessDeniedError, UserInvalidErrors } from '../Validation/Errors'
 
 const errorDeal = response => {
   switch (response.status) {
     case 200: return response.data 
     case 204: return []
+    case 401: throw new UserInvalidErrors()
     case 403: throw new AccessDeniedError()
     case 404: throw new NotFound() 
     default: throw new UnexpectedError()
