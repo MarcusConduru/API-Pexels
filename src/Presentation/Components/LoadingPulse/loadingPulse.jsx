def1f51ca21db39afff3a9dcf784b1ca1cee83ca
Marcus Vinicius <marcus.conduru@gmail.com>
2021-12-11 01:25:41 -0300

    feat: ensure add a new component loading

---
+++ src/Presentation/Components/LoadingPulse/loadingPulse.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+import './loadingPulse-styles.scss'
+
+const LoadingPulse = () => {
+  return (
+    <div className="Pulse__container">
+      <div className="Pulse__item" />
+    </div>
+  )
+}
+
+export default LoadingPulse
