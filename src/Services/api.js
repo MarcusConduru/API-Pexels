cad864ac87255369eeade0aebad9f9df7e24ba6c
Marcus Vinicius <marcus.conduru@gmail.com>
2021-12-11 21:37:09 -0300

    feat: API KEY

--- src/Services/api.js
+++ src/Services/api.js
@@ -3,8 +3,7 @@ import axios from 'axios'
 const api = axios.create({
   baseURL: 'https://api.pexels.com/v1',
   headers: {
-    authorization: '563492ad6f91700001000001a627014a4ed34c1286042d5326d0baed'
-    // Chave Da Api
+    authorization: 'API KEY'
   }
 })
 export default api
